b71fa30e4b6d90176c8bbb2eaa9505d1d0715acd
sanjaysneha <109469901+sanjayeedith@users.noreply.github.com>
2024-01-26 21:56:14 +0530

    Update ATS.py

--- ATS.py
+++ ATS.py
@@ -1,71 +1,87 @@
+from dotenv import load_dotenv
+
+load_dotenv()
 import base64
 import streamlit as st
 import os
 import io
-from PIL import Image
-import pytesseract  # For text extraction from images
+from PIL import Image 
+import pdf2image
 import google.generativeai as genai
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-def get_gemini_response(input, img_content, prompt):
-    model = genai.GenerativeModel('gemini-pro-vision')
-    response = model.generate_content([input, img_content[0], prompt])
+
+def get_gemini_response(input,pdf_cotent,prompt):
+    model=genai.GenerativeModel('gemini-pro-vision')
+    response=model.generate_content([input,pdf_content[0],prompt])
     return response.text
 
-def input_image_setup(uploaded_file):
+def input_pdf_setup(uploaded_file):
     if uploaded_file is not None:
-        # Read image data as bytes
-        img_byte_arr = uploaded_file.read()
+        ## Convert the PDF to image
+        images=pdf2image.convert_from_bytes(uploaded_file.read())
 
-        # Extract text from the image using pytesseract
-        text = pytesseract.image_to_string(Image.open(io.BytesIO(img_byte_arr)))
+        first_page=images[0]
 
-        img_parts = [
+        # Convert to bytes
+        img_byte_arr = io.BytesIO()
+        first_page.save(img_byte_arr, format='JPEG')
+        img_byte_arr = img_byte_arr.getvalue()
+
+        pdf_parts = [
             {
-                "mime_type": "text/plain",  # MIME type for text
-                "data": text
+                "mime_type": "image/jpeg",
+                "data": base64.b64encode(img_byte_arr).decode()  # encode to base64
             }
         ]
-        return img_parts
+        return pdf_parts
     else:
         raise FileNotFoundError("No file uploaded")
-st.set_page_config(page_title="ATS Resume Expert")
+
+## Streamlit App
+
+st.set_page_config(page_title="ATS Resume EXpert")
 st.header("ATS Tracking System")
-input_text = st.text_area("Job Description: ", key="input")
-uploaded_file = st.file_uploader("Upload your resume (image)...", type=["jpg", "jpeg", "png"])
+input_text=st.text_area("Job Description: ",key="input")
+uploaded_file=st.file_uploader("Upload your resume(PDF)...",type=["pdf"])
+
 
 if uploaded_file is not None:
-    st.write("Image Uploaded Successfully")
+    st.write("PDF Uploaded Successfully")
+
 
 submit1 = st.button("Tell Me About the Resume")
-submit3 = st.button("Percentage Match")
+
+#submit2 = st.button("How Can I Improvise my Skills")
+
+submit3 = st.button("Percentage match")
 
 input_prompt1 = """
-You are an experienced Technical Human Resource Manager. Your task is to review the provided resume against the job description. 
-Please share your professional evaluation on whether the candidate's profile aligns with the role. 
-Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
+ You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
+  Please share your professional evaluation on whether the candidate's profile aligns with the role. 
+ Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
 """
 
 input_prompt3 = """
-You are a skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality. 
-Your task is to evaluate the resume against the provided job description and give me the percentage of match. 
-Also, provide missing keywords and final thoughts.
+You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
+your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
+the job description. First the output should come as percentage and then keywords missing and last final thoughts.
 """
 
 if submit1:
     if uploaded_file is not None:
-        img_content = input_image_setup(uploaded_file)
-        response = get_gemini_response(input_prompt1, img_content, input_text)
-        st.subheader("The Response is")
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(input_prompt1,pdf_content,input_text)
+        st.subheader("The Repsonse is")
         st.write(response)
     else:
-        st.write("Please upload the resume")
+        st.write("Please uplaod the resume")
 
 elif submit3:
     if uploaded_file is not None:
-        img_content = input_image_setup(uploaded_file)
-        response = get_gemini_response(input_prompt3, img_content, input_text)
-        st.subheader("The Response is")
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(input_prompt3,pdf_content,input_text)
+        st.subheader("The Repsonse is")
         st.write(response)
     else:
-        st.write("Please upload the resume")
+        st.write("Please uplaod the resume")
